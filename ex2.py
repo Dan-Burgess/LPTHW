cf5c0f978b2f900430a282601f114c08bdd5a2b1
Dan-Burgess <ultradan@gmail.com>
2017-10-17 22:19:14 +1100

    More exercises - to ex 5

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#This is a comment
+
+print "I could have code running like this" #This bit doesn't run though
+
+#Use this to disable some parts of code
+#print "This won't run"
+
+print "This will run though"
+
